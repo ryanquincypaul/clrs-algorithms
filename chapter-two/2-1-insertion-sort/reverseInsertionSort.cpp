871f86cd5c02362fccd23633f1f6264cf8195349
ryan <littlefoot.paul@gmail.com>
2016-05-01 16:26:57 -0400

    add reverse insertion sort

---
+++ chapter-two/2-1-insertion-sort/reverseInsertionSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size) {
+	for ( int i = 0; i < size; i++ ) {
+		std::cout << arr[i] << ' ';
+	}
+	std::cout << endl;
+}
+
+int main()
+{
+	int A [6] = {31, 41, 59, 26, 41, 58};
+	int lengthOfA = sizeof(A) / sizeof(A[0]);
+	std::cout << "Pre reverse insertion sort" << endl;
+	printArray(A,lengthOfA);	
+	for ( int i = 1; i < lengthOfA; i++)
+	{
+		int key = A[i];
+		int j = i - 1;
+		while ( j > -1 and A[j] < key)
+		{
+			A[j + 1] = A[j];
+			j = j - 1;
+		}
+		A[j + 1] = key;
+	}
+	std::cout << "Post reverse insertion sort" << endl;
+	printArray(A, lengthOfA);	
+	return 0;
+} 
+
